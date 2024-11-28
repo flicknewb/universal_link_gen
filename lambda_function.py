7ee0a6c3ef02aec9324ccffce248e6187e27e59e
Morris, Tyler <tmorris@walkerinfo.com>
2024-11-28 09:01:01 -0800

    adding survey id

--- lambda_function.py
+++ lambda_function.py
@@ -7,34 +7,12 @@ import os
 
 API_TOKEN = os.getenv("API_TOKEN")
 DATACENTER = os.getenv("DATACENTER")
-# from datetime import datetime
-# from dateutil.relativedelta import relativedelta
-# from jsonschema import validate
-# from jsonschema.exceptions import ValidationError
-# # Updated schema
-# schema = {
-#     "type": "object",
-#     "properties": {
-#         "firstName": {"type": "string"},
-#         "lastName": {"type": "string"},
-#         "email": {"type": "string"},
-#         "phone": {"type": "string"},
-#         "extRef": {"type": "string"},
-#         "embeddedData": {"type": "object"},
-#         "transactionData": {"type": "object"},
-#         "DIRECTORY_ID": {"type": "string"},
-#         "MAILINGLIST_ID": {"type": "string"},
-#         "SURVEY_ID": {"type": "string"}
-#     },
-#     "required": ["DIRECTORY_ID", "MAILINGLIST_ID", "SURVEY_ID"]
-# }
-# AWS Lambda entry point
 
 
 def lambda_handler(event, context):
     print("Incoming Event:", event['body'], type(event['body']))
     body = json.loads(event['body'])
-    sids = ['']
+    sids = ['SV_3xhKFtpxNaytUYm']
     Credentials().qualtrics_api_credentials(token=API_TOKEN,
                                             data_center=DATACENTER)
     r = Responses()
