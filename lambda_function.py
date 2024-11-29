b4b21cfea21d0994d02ae17b2466ef28887b920a
Morris, Tyler <tmorris@walkerinfo.com>
2024-11-29 09:30:30 -0800

    debug code - dumps

--- lambda_function.py
+++ lambda_function.py
@@ -101,7 +101,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"misses": misses}),
+        "body": json.dumps({}),
     }
 
 
